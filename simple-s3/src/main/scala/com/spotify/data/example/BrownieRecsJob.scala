6eb24e22f0e5d95346fce63625e2c8b868a7be4b
Rafal Wojdyla <rav@spotify.com>
2018-03-13 15:40:13 -0400

    add comment

--- simple-s3/src/main/scala/com/spotify/data/example/BrownieRecsJob.scala
+++ simple-s3/src/main/scala/com/spotify/data/example/BrownieRecsJob.scala
@@ -7,6 +7,7 @@ object BrownieRecsJob {
   def main(cmdlineArgs: Array[String]): Unit = {
     val (sc, args) = ContextAndArgs(cmdlineArgs)
 
+    // afair the / at the end of save path is significant
     sc.parallelize(1 to 10)
       .saveAsTextFile("s3a://BUCKET/ravtest/")
 
